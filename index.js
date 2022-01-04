0133acfd978f88620bd6431a69a85ed100a5b652
luis miranda <luismiguel1115@gmail.com>
2022-01-04 10:17:03 -0500

    api frame as pi

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // esto es una prueba de brnach Mauro
+const server = require('./src/app.js');
+const { conn } = require('./src/db.js');
+
+// Syncing all the models at once.
+conn.sync({ force: true }).then(() => {
+  server.listen(3001, () => {
+    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  });
+});
