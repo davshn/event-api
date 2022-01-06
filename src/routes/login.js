1e004a0c64b19b23266afcce9a72d4f54d5298e8
danielaroldanb <89650193+danielaroldanb@users.noreply.github.com>
2022-01-06 13:11:09 -0500

    login

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,8 +1,41 @@
 const { Router } = require("express");
-const axios = require("axios");
-
+const { User } = require("../db.js");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const router = Router();
 
+const {
+    TOKEN_KEY
+  } = process.env;
 
+router.post("/", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!(email && password)) {
+      res.status(400).send("All input is required");
+    }
+
+    const user = await User.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const token = jwt.sign(
+        { id: user.id, email: user.email },
+        TOKEN_KEY,
+        {
+          expiresIn: "2h",
+        }
+      );
+
+      user.token = token;
+     
+      res.status(200).json(token);
+    } else {
+      res.status(400).send("El usuario no existe");
+    }
+  } catch (error) {
+    res.status(400).send("Error en el login");
+  }
+});
 
 module.exports = router;
