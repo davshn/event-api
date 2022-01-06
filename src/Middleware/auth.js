61220a3e5bd1c18b2197303167106decde935cdf
Hernán David Figueroa <davshn@gmail.com>
2022-01-06 15:53:22 -0500

    Token key

--- src/Middleware/auth.js
+++ src/Middleware/auth.js
@@ -12,7 +12,7 @@ const verifyToken = (req, res, next) => {
       return res.status(403).send("A token is required for authentication");
     }
     try {
-      const decoded = jwt.verify(token, config.TOKEN_KEY);
+      const decoded = jwt.verify(token, process.env.TOKEN_KEY);
       req.user = decoded;
     } catch (err) {
       return res.status(401).send("Invalid Token");
