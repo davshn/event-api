76914eeb77bef09a5e03c90f5d1126a6d0487d6a
Hernán David Figueroa <davshn@gmail.com>
2022-01-07 11:03:00 -0500

    Mesage when not match event

--- src/routes/event.js
+++ src/routes/event.js
@@ -4,10 +4,12 @@ const { Event } = require("../db")
 const router = Router();
 
 router.get('/:eventId', async function(req, res) {
+    console.log(req.params)
     const eventId = req.params.eventId;
+
     const event = await Event.findAll({where:{id:eventId}});
 
-    event
+    event.length
     ?res.json(event)
     :res.json({message: "No event found for that id"});
 });
