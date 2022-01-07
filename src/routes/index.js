d48536b03b6947448a20138e57a7c78599dedf4f
Hernán David Figueroa <davshn@gmail.com>
2022-01-07 11:02:05 -0500

    import event route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
 const { Router } = require("express");
 
 const register=require("./register")
-const login=require("./login")
+const login = require("./login")
+const event = require("./event")
 const events=require("./events")
 const categories=require("./categories")
 const router = Router();
@@ -9,7 +10,7 @@ const router = Router();
 
 router.use("/register",register)
 router.use("/login",login)
-router.use("/event",events)
+router.use("/event",event)
 router.use("/events",events)
 router.use("/categories",categories)
 
