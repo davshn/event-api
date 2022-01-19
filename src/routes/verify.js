6d4e7f699bbb87ba77637d006b85dd756e90532b
Hernán David Figueroa <davshn@gmail.com>
2022-01-19 00:18:24 -0500

    user verification

--- src/routes/verify.js
+++ src/routes/verify.js
@@ -1,11 +1,21 @@
 const { Router } = require("express");
-const { Category } = require("../db.js");
+const { User } = require("../db.js");
 const router = Router();
 
 router.get("/",  async (req, res) => {
 
   try {
-  console.log(req.query)
+    const email = req.query.mail.toLowerCase();
+    const verificationId = req.query.id;
+    const user = await User.findOne({ where: { email } });
+    
+    if (user.verificationCode === verificationId) {
+      await User.update({ where: { email }, user: { verifyProfile: true } });
+      res.status(200).send("Usuario Verificado");
+    }
+    
+    else res.status(404).send("Usuario no encontrado");
+    
   } catch (error) {
     res.status(400).send("Error en la carga de categorias");
   }
