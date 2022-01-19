0991c20a064889675f108516adc2b6289a327be4
Hernán David Figueroa <davshn@gmail.com>
2022-01-19 00:21:19 -0500

    user verification

--- src/routes/verify.js
+++ src/routes/verify.js
@@ -10,6 +10,7 @@ router.get("/",  async (req, res) => {
     const user = await User.findOne({ where: { email } });
     
     if (user.verificationCode === verificationId) {
+      console.log("Usuario encontrado")
       await User.update({ where: { email }, user: { verifyProfile: true } });
       res.status(200).send("Usuario Verificado");
     }
@@ -17,7 +18,7 @@ router.get("/",  async (req, res) => {
     else res.status(404).send("Usuario no encontrado");
     
   } catch (error) {
-    res.status(400).send("Error en la carga de categorias");
+    res.status(400).send("Error en la verificacion");
   }
 });
 
